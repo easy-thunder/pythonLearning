2fa56e8b9a0d6549c0a7239419a484a86c1dfa39
Jacob David Diehl <jakediehl17@gmail.com>
2023-05-01 09:04:04 -0600

    add while loops

---
+++ lib/app.py
@@ -0,0 +1,60 @@
+# import random
+
+# random_number = random.randint(0,10)
+
+
+
+# guess_count=1
+# guess_limit = 3
+
+# while guess_count<=guess_limit:
+#     guess=int(input("take a guess: "))
+#     guess_count+=1
+#     if guess == random_number:
+#         print("you won")
+#         break
+    
+# else:
+#         print("max guess limit")
+
+# game=""
+# started=False 
+# stopped=False
+
+# while True:
+#     game = input(" ").lower()
+#     if game=="start" and started==True:
+#         print("car is already started")
+#     elif game=="start" and started==False:
+#         print("car started")
+#         started=True
+#         stopped=False
+#     elif game=="stop" and stopped==True:
+#         print("car is already stopped")
+#     elif game =="stop" and stopped==False:
+#         print("car stopped")
+#         started==False
+#         stopped==True
+#     elif game =="quit":
+#         break
+#     elif game == "help":
+#         print("start - to start the car")
+#         print("stop - to stop the car")
+#         print("quit - to exit")
+#     else:
+#         print("sorry didn't understand")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
